1cf9fac272cf805bf690e7d87599c93bd192d3f8
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 14:54:01 +0100

    added Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <Header />
+  <div>
+    <Header />
+    <Main />
+  </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
 export default {
   components: {
     Header,
+    Main,
   },
 };
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <jumbotron id="content">
+      <div class="container">CONTENT GOES HERE</div>
+    </jumbotron>
+    <div>
+      <div id="blue-content">
+        <div class="container">ulteriore contenuto andrà qua</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#content {
+  background-color: black;
+  color: white;
+  height: 100px;
+  display: flex;
+  align-items: center;
+}
+
+#blue-content {
+  background-color: #0082f9;
+  height: 100px;
+  display: flex;
+  align-items: center;
+}
+</style>
