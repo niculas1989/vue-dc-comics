1b2eaab8e3bb181cc18fd0c1101a922ebfdef5af
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 14:56:56 +0100

    added Footer.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <Header />
     <Main />
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
 export default {
   components: {
     Header,
     Main,
+    Footer,
   },
 };
 </script>
