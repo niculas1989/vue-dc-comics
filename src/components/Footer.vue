1b975933d7945f522ca76acfd44ff68e56bbe93a
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 15:51:33 +0100

    added Footer information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,55 @@
 <template>
   <div>
-    <div id="upper-footer"></div>
+    <div id="upper-footer">
+      <div class="container">
+        <div>
+          <h4>DC COMICS</h4>
+          <ul>
+            <li><a href="#">Charatcters</a></li>
+            <li><a href="#">Comics</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Games</a></li>
+            <li><a href="#">Videos</a></li>
+            <li><a href="#">News</a></li>
+          </ul>
+        </div>
+        <div>
+          <h4>SHOP</h4>
+          <ul>
+            <li><a href="#">Shop DC</a></li>
+            <li><a href="#">Shop DC Collectibles</a></li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h4>DC</h4>
+        <ul>
+          <li><a href="#">Terme Of Use</a></li>
+          <li><a href="#">Privacy policy (New)</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Ad Choices</a></li>
+          <li><a href="#">Advertising</a></li>
+          <li><a href="#">Jobs</a></li>
+          <li><a href="#">Talent Workshops</a></li>
+          <li><a href="#">CPSC Certificates</a></li>
+          <li><a href="#">Ratings</a></li>
+          <li><a href="#">Shop Help</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div>
+        <h4>SITES</h4>
+        <ul>
+          <li><a href="#">DC</a></li>
+          <li><a href="#">MAD Magazine</a></li>
+          <li><a href="#">DC Kids</a></li>
+          <li><a href="#">DC Universe</a></li>
+          <li><a href="#">DC Pover Visa</a></li>
+        </ul>
+      </div>
+    </div>
+
     <div id="bottom-footer">
       <div class="container">
         <div class="wrapper">
@@ -23,10 +72,13 @@
 export default {};
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 #upper-footer {
   height: 300px;
   background-image: url(../assets/img/footer-bg.jpg);
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
 }
 
 #bottom-footer {
@@ -42,5 +94,19 @@ export default {};
 img {
   height: 20px;
   padding: 0 10px;
+}
+h4 {
+  color: white;
+}
+
+ul {
+  display: flex;
+  flex-direction: column;
+  li {
+    list-style: none;
+    a {
+      text-decoration: none;
+    }
+  }
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -26,7 +26,7 @@
 export default {};
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 header {
   background-color: white;
 }
