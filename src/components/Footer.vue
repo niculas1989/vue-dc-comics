df70ed86bb8cfc163fdb267186132e3b86a8c1b6
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 15:02:53 +0100

    added style in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>piccola prova</div>
+  <div>
+    <div id="upper-footer"></div>
+    <div id="bottom-footer"></div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +10,13 @@ export default {};
 </script>
 
 <style>
+#upper-footer {
+  height: 300px;
+  background-image: url(../assets/img/footer-bg.jpg);
+}
+
+#bottom-footer {
+  height: 100px;
+  background-color: #303030;
+}
 </style>
