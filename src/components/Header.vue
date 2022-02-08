a394165f59c59390053977edcc25f0544495562d
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 14:43:32 +0100

    added right style for Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,24 @@
 <template>
-  <header class="container">
-    <figure>
-      <img src="../assets/img/dc-logo.png" alt="" />
-    </figure>
-    <ul>
-      <li><a href="#">Characters</a></li>
-      <li><a href="#">COMICS</a></li>
-      <li><a href="#">MOVIES</a></li>
-      <li><a href="#">TV</a></li>
-      <li><a href="#">GAMES</a></li>
-      <li><a href="#">COLLECTIBLES</a></li>
-      <li><a href="#">VIDEOS</a></li>
-      <li><a href="#">FANS</a></li>
-      <li><a href="#">NEWS</a></li>
-      <li><a href="#">SHOP</a></li>
-    </ul>
+  <header>
+    <div class="container">
+      <div id="nav">
+        <figure>
+          <img src="../assets/img/dc-logo.png" alt="" />
+        </figure>
+        <ul>
+          <li><a href="#">Characters</a></li>
+          <li><a href="#">COMICS</a></li>
+          <li><a href="#">MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </div>
+    </div>
   </header>
 </template>
 
@@ -24,8 +28,14 @@ export default {};
 
 <style lang="scss">
 header {
+  background-color: white;
+}
+
+#nav {
+  margin-top: 40px;
   display: flex;
   justify-content: space-between;
+  align-items: center;
 }
 
 ul {
