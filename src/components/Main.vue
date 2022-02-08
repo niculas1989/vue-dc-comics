5a529a2f55828edead9a0f915ee7eff37b006376
niculas1989 <nicolas.maranzano@libero.it>
2022-02-08 17:24:15 +0100

    added more info for Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,7 +5,40 @@
     </jumbotron>
     <div>
       <div id="blue-content">
-        <div class="container">ulteriore contenuto andrà qua</div>
+        <div class="container">
+          <div id="merchandising">
+            <figure>
+              <a href="#">
+                <img src="../assets/img/buy-comics-digital-comics.png" alt="" />
+                <span>DIGITAL COMICS</span>
+              </a>
+            </figure>
+            <figure>
+              <a href="#">
+                <img src="../assets/img/buy-comics-merchandise.png" alt="" />
+                <span>DC MERCHANDISE</span>
+              </a>
+            </figure>
+            <figure>
+              <a href="#">
+                <img src="../assets/img/buy-comics-subscriptions.png" alt="" />
+                <span>SUBSCRIPTION</span>
+              </a>
+            </figure>
+            <figure>
+              <a href="#">
+                <img src="../assets/img/buy-comics-shop-locator.png" alt="" />
+                <span>COMIC SHOP LOCATOR</span>
+              </a>
+            </figure>
+            <figure>
+              <a href="#">
+                <img src="../assets/img/buy-dc-power-visa.svg" alt="" />
+                <span>DC POWER VISA</span>
+              </a>
+            </figure>
+          </div>
+        </div>
       </div>
     </div>
   </div>
